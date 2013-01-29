9f2804ebfef1e6b738b3c1e1e8858da91658bcef
London Ruby User Group <lrug@jasper.dreamhost.com>
2013-01-29 01:01:05 -0800

    Add workaround for rails 2.3.16

--- config/initializers/rails-security-fix.rb
+++ config/initializers/rails-security-fix.rb
@@ -1,2 +1,5 @@
+# 2.3.15 workaround 
 ActionController::Base.param_parsers.delete(Mime::XML)
 ActionController::Base.param_parsers.delete(Mime::YAML)
+# 2.3.16 workaround
+ActiveSupport::JSON.backend = "JSONGem"
