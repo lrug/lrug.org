e01935763eadc5d6c4eab8ec6d286becb173e626
Murray Steele <muz@h-lame.com>
2014-07-15 23:01:18 +0100

    Create some more helpers, enough to give us a working index
    Ish, obviously we need to work on ordering.  We've also moved the index.html.md out of source/lrug_root and into source/ (mostly so it won't get clobbered when we re-generate the data).

--- lib/lrug_helpers.rb
+++ lib/lrug_helpers.rb
@@ -11,4 +11,25 @@ module LRUGHelpers
     end
   end
 
+  def with_page(slug_match, &html_block)
+    page = sitemap.where(:slug => slug_match).first
+    if page
+      concat_content(capture_html page, &html_block)
+    end
+  end
+
+  def with_children_of(page, args = {}, &html_block)
+    all_children = page.children
+    start = args[:offset] || 0
+    stop = if args[:limit]
+        start + args[:limit] -1
+      else
+        -1
+      end
+    all_children[start..stop].each { |child| concat_content(capture_html(child, &html_block)) }
+  end
+
+  def date_format(date, format)
+    date.strftime(format)
+  end
 end
