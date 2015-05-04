1d977df23612b67baf3d36a15dfa915664192176
Murray Steele <muz@h-lame.com>
2015-05-04 13:23:20 +0100

    Remove if_url helper now that it's not used

--- lib/lrug_helpers.rb
+++ lib/lrug_helpers.rb
@@ -3,12 +3,6 @@ module LRUGHelpers
     yield_content(:title) || for_page.data.title
   end
 
-  def if_url(matches, &html_block)
-    if current_resource.url =~ matches
-      concat_content(capture_html &html_block)
-    end
-  end
-
   def rss_path(for_page = current_page)
     "/rss/#{yield_content :rss_path}"
   end
