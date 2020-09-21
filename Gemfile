d363accec6b3b891172be2ddbd67f6e80f43b3d7
Mike Rogers <me@mikerogers.io>
2020-09-21 17:53:05 +0100

    Minifying HTML & CSS

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://www.rubygems.org'
 ruby "~> 2.4"
 
 gem 'middleman'
+gem 'middleman-minify-html'
 gem 'builder'
 gem 'pry'
 gem 'git'
--- config.rb
+++ config.rb
@@ -22,6 +22,26 @@ set :markdown, input: 'LRUGExtendedKramdown'
 
 configure :build do
   ignore 'archive/*'
+
+  activate :minify_css
+  activate :minify_html do |html|
+    html.remove_multi_spaces        = true   # Remove multiple spaces
+    html.remove_comments            = true   # Remove comments
+    html.remove_intertag_spaces     = false  # Remove inter-tag spaces
+    html.remove_quotes              = false  # Remove quotes
+    html.simple_doctype             = false  # Use simple doctype
+    html.remove_script_attributes   = false  # Remove script attributes
+    html.remove_style_attributes    = true   # Remove style attributes
+    html.remove_link_attributes     = true   # Remove link attributes
+    html.remove_form_attributes     = false  # Remove form attributes
+    html.remove_input_attributes    = false  # Remove input attributes
+    html.remove_javascript_protocol = true   # Remove JS protocol
+    html.remove_http_protocol       = false  # Remove HTTP protocol
+    html.remove_https_protocol      = false  # Remove HTTPS protocol
+    html.preserve_line_breaks       = false  # Preserve line breaks
+    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+    html.preserve_patterns          = nil    # Patterns to preserve
+  end
 end
 
 page '/book-reviews/index.html', layout: 'books'
