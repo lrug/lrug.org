92b581b49abad269e0893a42dbbad522a2034d22
Mike Rogers <me@mikerogers.io>
2020-12-05 19:58:19 +0000

    Ditching the "load info from GitHub API" approach

--- lib/speaker_helpers.rb
+++ lib/speaker_helpers.rb
@@ -24,24 +24,7 @@ module SpeakerHelpers
     def initialize(name, speaker_data)
       @name = name
       @talks = speaker_data[:talks]
-      
-      if speaker_data[:profile][:github_username].present?
-        @profile = OpenStruct.new github_profile(speaker_data[:profile][:github_username])
-      else
-        @profile = OpenStruct.new speaker_data[:profile]
-      end
-    end
-
-    private
-
-    def github_profile(username)
-      @@github_users ||= {}
-      @@github_users[username] ||= load_github_profile_from_api(username)
-    end
-
-    def load_github_profile_from_api(username)
-      response = HTTParty.get("https://api.github.com/users/#{username}")
-      JSON.parse(response.body, symbolize_names: true)
+      @profile = OpenStruct.new speaker_data[:profile]
     end
   end
 end
