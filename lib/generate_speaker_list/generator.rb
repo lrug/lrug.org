a2e0635a72234978ed44d613d23709ab97b947f1
Mike Rogers <me@mikerogers.io>
2020-12-06 22:19:34 +0000

    Outputting speakers list to JSON file - the format is still wrong, but it's looking good

--- lib/generate_speaker_list/generator.rb
+++ lib/generate_speaker_list/generator.rb
@@ -1,16 +1,34 @@
+require 'json'
+
 module GenerateSpeakerList
   class Generator
+    SPEAKERS_DATA_FILE = 'data/speakers.json'
+
     def initialize
-      @files = Dir['source/meetings/*/*/index.html.md'].sort
-      @authors = []
+      @files = Dir['source/meetings/*/*/index.html.md'].sort.reverse
+      @talks = []
     end
 
     def call
-      @files.each do |file|
-        @authors << ParseFile.new(file).call
-      end
+      load_all_talks_from_files!
 
-      pp @authors
+      @talks = @talks.flatten.group_by { |talk| talk[:author] }
+
+      pp @talks
+
+      save_talks_to_json!
+    end
+
+    private
+
+    def load_all_talks_from_files!
+      @files.each do |file|
+        @talks << ParseFile.new(file).call
+      end
+    end
+
+    def save_talks_to_json!
+      File.write(SPEAKERS_DATA_FILE, JSON.pretty_generate(@talks))
     end
   end
 end
