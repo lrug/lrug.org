76a5873ed223e4060729dd3bb09c451303867a48
Cecily Rowland <cec1rowland@gmail.com>
2023-03-21 13:44:31 +0000

    Scraped all video links

--- lib/video_link_scraper.rb
+++ lib/video_link_scraper.rb
@@ -3,25 +3,44 @@ require "nokogiri"
 require 'yaml'
 
 class VideoLinkScraper
+  BASE_URL = 'https://skillsmatter.com'
+
   def initialize
-    @url = 'https://skillsmatter.com/skillscasts/14569-concurrency-in-crystal'
     @filepath = './data/coverage/scraped_coverage.yml'
+    @page_numbers = 0..38
   end 
 
   def self.call
     @scraper = VideoLinkScraper.new
-    @scraper.scrape
+    @scraper.skillsmatter_scrape
   end
 
-  def scrape
-    html = URI.open(@url)
+  def skillsmatter_scrape
+    talk_links = []
+    # just for testing - replace [0] with @page_numbers
+    [0].each do |number|
+      page_url = "#{BASE_URL}/groups/3-london-ruby-user-group?page=#{number}&#past_events"
+      
+      html = URI.open(page_url)
+      document = Nokogiri::HTML(html)
+
+      talk_links << document.css("div.event-info").map do |node| 
+				             BASE_URL + node.css("a").first.attr("href")
+                   end
+    end
+
+    talk_links.flatten!
+  end
+
+  def video_info_scrape(url)
+    html = URI.open(url)
     doc = Nokogiri::HTML(html)
   
     title = doc.css('h1').text
     date = doc.css('span.keydetails__datesdrawertrigger').text
 
     month, year = split_month_and_year(date)
-    add_to_yaml_file(title, month, year, @url)
+    add_to_yaml_file(title, month, year, url)
   end
 
   private
