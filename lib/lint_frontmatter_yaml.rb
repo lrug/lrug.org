080d777491a8da3cc482583143b1e4461e527946
Murray Steele <muz@h-lame.com>
2026-04-01 23:11:10 +0100

    Implement ignore file for our frontmatter linter
    Like yamllint it reads from `.gitignore` and `.yamlignore` and ignores any
    files that match patterns in there. This means using `Dir.glob` so we can pass
    arguments, and `File.fnmatch?` to do the actual globbing. We can avoid multiple
    questions by combining all the patterns in each file into a single
    `{pattern_1,pattern_2}` glob and letting the file system work it out.
    
    It's not a straightforward "entry in `.gitignore`" -> "entry in glob" filter
    though, we do need to do some manipulation and duplication. Doing a full
    implementation of `.gitignore` files is outside the remit of this, but we've
    implemented enough to make it work. In particular we don't implement negative
    matches.

--- lib/lint_frontmatter_yaml.rb
+++ lib/lint_frontmatter_yaml.rb
@@ -23,11 +23,16 @@ class LintFrontmatterYaml
     frontmatter.join
   end
 
+  def ignore_glob
+    @ignore_glob ||= compile_ignore_glob
+  end
+
   def main
     success = true
-    Dir["source/**/*"].each do |f|
+    Dir.glob("source/**/*", File::FNM_EXTGLOB | File::FNM_DOTMATCH).each do |f|
       next unless File.file? f
       next unless starts_with_yaml? f
+      next if File.fnmatch? ignore_glob, f, File::FNM_EXTGLOB | File::FNM_DOTMATCH
 
       Open3.popen2("yamllint -") do |stdin, stdout_and_stderr, wait_thread|
         frontmatter = read_frontmatter(f)
@@ -40,6 +45,44 @@ class LintFrontmatterYaml
       end
     end
     exit success
+  end
+
+  private
+
+  def compile_ignore_glob
+    patterns = File.read(".gitignore") + File.read(".yamlignore")
+    negative_entries, positive_entries = patterns
+      .lines
+      .reject { it.starts_with? "#" }
+      .reject(&:blank?)
+      .map(&:chomp)
+      .partition { it.starts_with? "!" }
+    puts "WARNING: can't (currently) deal with negative gitignore patterns" if negative_entries.any?
+    "{#{positive_entries.map { gitignore_to_glob(it) }.join(',')}}"
+  end
+
+  def gitignore_to_glob(path)
+    # TODO: https://git-scm.com/docs/gitignore#_pattern_format
+    # 1. entries starting / needs to have it removed, globbing _already_ anchors
+    #    at the start
+    # 2. entries not starting / need to be duplicated and prefixed with **/ to
+    #    pick up the exact path in the root and any descendant path
+    paths = if path.starts_with? "/"
+              [path.delete_prefix("/")]
+            elsif path.starts_with? "**/"
+              # do nothing
+              [path]
+            else
+              [path, "**/#{path}"]
+            end
+
+    # 3. entries ending in / need to have it duplicated and augmented with /**
+    #    to capture files in subdirs
+    # 4. our usecase only cares about files otherwise we'd need to duplicate
+    #    and trim `/` too)
+    paths = paths.map { [it.delete_suffix("/"), "#{it.delete_suffix('/')}/**"] } if path.ends_with? "/"
+
+    paths.join(",")
   end
 end
 
